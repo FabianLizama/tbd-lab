e36b01825a311cd61c494ad4d7ddd6666d0062fa
Jaime Riquelme <jaime.riquelme@usach.cl>
2024-04-09 21:48:29 -0400

    Update DatabaseContext

--- api/src/main/java/org/tbd/fifth/group/volunteer/services/DatabaseContext.java
+++ api/src/main/java/org/tbd/fifth/group/volunteer/services/DatabaseContext.java
@@ -29,28 +29,7 @@ public class DatabaseContext {
     public Sql2o sql2o(){
         return new Sql2o(dbUrl, dbUser, dbPass);
     }
+    
 
-    @Bean
-    public SecretKey secretKey() {
-        return Jwts.SIG.HS256.key().build();
-    }
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        // Configure the date format for deserialization globally
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        objectMapper.setDateFormat(dateFormat);
-
-        // If needed, you can configure other ObjectMapper settings here
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-        // Register a custom date deserializer if needed
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(Date.class, new DateDeserializers.DateDeserializer());
-        objectMapper.registerModule(module);
-
-        return objectMapper;
-    }
 }
