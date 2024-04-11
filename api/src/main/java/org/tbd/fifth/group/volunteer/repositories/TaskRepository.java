e499ba91515f04eef3224814ba1df9df65a9d5cc
Jaime Riquelme <jaime.riquelme@usach.cl>
2024-04-11 17:20:05 -0400

    Update TaskServices

--- api/src/main/java/org/tbd/fifth/group/volunteer/repositories/TaskRepository.java
+++ api/src/main/java/org/tbd/fifth/group/volunteer/repositories/TaskRepository.java
@@ -2,9 +2,27 @@ package org.tbd.fifth.group.volunteer.repositories;
 
 import org.tbd.fifth.group.volunteer.models.TaskModel;
 
+import java.util.List;
+
 public interface TaskRepository {
     public TaskModel createTask(TaskModel task);
 
     public TaskModel getTask(int task_id);
+
+    public List<TaskModel> getAllTasks(String token);
+
+    public List<TaskModel> getTasksByEmergencyId(int emergency_id);
+
+    public int countActiveTasksByEmergencyId(int id);
+
+    public int countAllActiveTasks();
+
+    public boolean updateTask(TaskModel task);
+
+    public boolean deleteTask(int task_id);
+
+    public boolean deleteAllTasks();
+
+
 
 }
--- api/src/main/java/org/tbd/fifth/group/volunteer/services/TaskService.java
+++ api/src/main/java/org/tbd/fifth/group/volunteer/services/TaskService.java
@@ -7,11 +7,16 @@ import org.sql2o.Sql2o;
 import org.tbd.fifth.group.volunteer.models.TaskModel;
 import org.tbd.fifth.group.volunteer.repositories.TaskRepository;
 
+import java.util.List;
+
 @Repository
 public class TaskService implements TaskRepository{
 
     @Autowired
     private Sql2o sql2o;
+
+    @Autowired
+    private JwtMiddlewareServices JWT;
 
     @Override
     public TaskModel createTask(TaskModel task){
@@ -40,4 +45,100 @@ public class TaskService implements TaskRepository{
             return null;
         }
     }
+
+    @Override
+    public List<TaskModel> getAllTasks(String token){
+        if(JWT.validateToken(token)){
+            try(Connection connection = sql2o.open()){
+                return connection.createQuery("SELECT * FROM \"task\"")
+                        .executeAndFetch(TaskModel.class);
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+                return null;
+            }
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public List<TaskModel> getTasksByEmergencyId(int emergency_id){
+        try(Connection connection = sql2o.open()){
+            return connection.createQuery("SELECT * FROM \"task\" WHERE emergency_id = :emergency_id")
+                    .addParameter("emergency_id", emergency_id)
+                    .executeAndFetch(TaskModel.class);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public int countActiveTasksByEmergencyId(int id){
+        int activeTasks = 0;
+        try(Connection connection = sql2o.open()){
+            activeTasks = connection
+                    .createQuery("SELECT COUNT(*) FROM \"task\" WHERE emergency_id = :id AND task_state_id = '1'")
+                    .addParameter("id", id)
+                    .executeScalar(Integer.class);
+        }
+        return activeTasks;
+    }
+
+    @Override
+    public int countAllActiveTasks(){
+        int allActiveTasks = 0;
+        try(Connection connection = sql2o.open()){
+            allActiveTasks = connection
+                    .createQuery("SELECT COUNT(*) FROM \"task\" WHERE task_state_id = '1'")
+                    .executeScalar(Integer.class);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return 0;
+        }
+        return allActiveTasks;
+    }
+
+    @Override
+    public boolean updateTask(TaskModel task){
+        try(Connection connection = sql2o.open()){
+            connection.createQuery("UPDATE \"task\" SET emergency_id = :emergency_id, task_state_id = :task_state_id, task_name = :task_name WHERE task_id = :task_id")
+                    .addParameter("emergency_id", task.getEmergency_id())
+                    .addParameter("task_state_id", task.getTask_state_id())
+                    .addParameter("task_name", task.getTask_name())
+                    .addParameter("task_id", task.getTask_id())
+                    .executeUpdate();
+            return true;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteTask(int task_id){
+        try(Connection connection = sql2o.open()){
+            connection.createQuery("DELETE FROM \"task\" WHERE task_id = :task_id")
+                    .addParameter("task_id", task_id)
+                    .executeUpdate();
+            return true;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteAllTasks(){
+        try(Connection connection = sql2o.open()){
+            connection.createQuery("DELETE FROM \"task\"")
+                    .executeUpdate();
+            return true;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+
 }
