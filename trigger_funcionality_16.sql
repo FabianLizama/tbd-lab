9b9efdc26f58bd2e9d40f168668d3dc27d9cca4d
CristobalTorresU <cristobal.torres.u@usach.cl>
2024-04-18 00:35:13 -0400

    Fix CREATE TRIGGER Functionality 16

--- trigger_funcionality_16.sql
+++ trigger_funcionality_16.sql
@@ -33,61 +33,61 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- coordinator
-CREATE TRIGGER query_log_trigger
+CREATE TRIGGER query_log_trigger_coordinator
 AFTER INSERT OR UPDATE OR DELETE ON coordinator
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
 
 -- emergency
-CREATE TRIGGER query_log_trigger
+CREATE TRIGGER query_log_trigger_emergency
 AFTER INSERT OR UPDATE OR DELETE ON emergency
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
 
 -- institution
-CREATE TRIGGER query_log_trigger
+CREATE TRIGGER query_log_trigger_institution
 AFTER INSERT OR UPDATE OR DELETE ON institution
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
 
 -- ranking
-CREATE TRIGGER query_log_trigger
+CREATE TRIGGER query_log_trigger_ranking
 AFTER INSERT OR UPDATE OR DELETE ON ranking
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
 
 -- skill
-CREATE TRIGGER query_log_trigger
+CREATE TRIGGER query_log_trigger_skill
 AFTER INSERT OR UPDATE OR DELETE ON skill
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
 
 -- task
-CREATE TRIGGER query_log_trigger
+CREATE TRIGGER query_log_trigger_task
 AFTER INSERT OR UPDATE OR DELETE ON task
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
 
 -- task_state
-CREATE TRIGGER query_log_trigger
+CREATE TRIGGER query_log_trigger_task_state
 AFTER INSERT OR UPDATE OR DELETE ON task_state
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
 
 -- type_user
-CREATE TRIGGER query_log_trigger
+CREATE TRIGGER query_log_trigger_type_user
 AFTER INSERT OR UPDATE OR DELETE ON type_user
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
 
 -- userm
-CREATE TRIGGER query_log_trigger
+CREATE TRIGGER query_log_trigger_userm
 AFTER INSERT OR UPDATE OR DELETE ON userm
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
 
 -- volunteer
-CREATE TRIGGER query_log_trigger
+CREATE TRIGGER query_log_trigger_volunteer
 AFTER INSERT OR UPDATE OR DELETE ON volunteer
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
