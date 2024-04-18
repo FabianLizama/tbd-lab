089526b54913ee8751e7cba722e9c4fbecb89477
CristobalTorresU <cristobal.torres.u@usach.cl>
2024-04-18 00:31:36 -0400

    Trigger 16 & QueryLogController

--- trigger_funcionality_16.sql
+++ trigger_funcionality_16.sql
@@ -7,10 +7,9 @@ DECLARE
     user_name_m TEXT;
     query_type_m TEXT;
 BEGIN
-    -- Obtain time and date
     currentDate := CURRENT_DATE;
     currentTime := CURRENT_TIME;
-    --SELECT CURRENT_USER into user_name_m;
+    SELECT CURRENT_USER into user_name_m;
 
     IF TG_OP = 'INSERT' THEN
         query_type_m := 'Inserción';
@@ -33,8 +32,62 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- coordinator
 CREATE TRIGGER query_log_trigger
--- TODO: Cambiar para que funcione en todas las tablas.
-AFTER INSERT OR UPDATE OR DELETE ON ALL TABLES
+AFTER INSERT OR UPDATE OR DELETE ON coordinator
+FOR EACH STATEMENT
+EXECUTE FUNCTION query_modification_log();
+
+-- emergency
+CREATE TRIGGER query_log_trigger
+AFTER INSERT OR UPDATE OR DELETE ON emergency
+FOR EACH STATEMENT
+EXECUTE FUNCTION query_modification_log();
+
+-- institution
+CREATE TRIGGER query_log_trigger
+AFTER INSERT OR UPDATE OR DELETE ON institution
+FOR EACH STATEMENT
+EXECUTE FUNCTION query_modification_log();
+
+-- ranking
+CREATE TRIGGER query_log_trigger
+AFTER INSERT OR UPDATE OR DELETE ON ranking
+FOR EACH STATEMENT
+EXECUTE FUNCTION query_modification_log();
+
+-- skill
+CREATE TRIGGER query_log_trigger
+AFTER INSERT OR UPDATE OR DELETE ON skill
+FOR EACH STATEMENT
+EXECUTE FUNCTION query_modification_log();
+
+-- task
+CREATE TRIGGER query_log_trigger
+AFTER INSERT OR UPDATE OR DELETE ON task
+FOR EACH STATEMENT
+EXECUTE FUNCTION query_modification_log();
+
+-- task_state
+CREATE TRIGGER query_log_trigger
+AFTER INSERT OR UPDATE OR DELETE ON task_state
+FOR EACH STATEMENT
+EXECUTE FUNCTION query_modification_log();
+
+-- type_user
+CREATE TRIGGER query_log_trigger
+AFTER INSERT OR UPDATE OR DELETE ON type_user
+FOR EACH STATEMENT
+EXECUTE FUNCTION query_modification_log();
+
+-- userm
+CREATE TRIGGER query_log_trigger
+AFTER INSERT OR UPDATE OR DELETE ON userm
+FOR EACH STATEMENT
+EXECUTE FUNCTION query_modification_log();
+
+-- volunteer
+CREATE TRIGGER query_log_trigger
+AFTER INSERT OR UPDATE OR DELETE ON volunteer
 FOR EACH STATEMENT
 EXECUTE FUNCTION query_modification_log();
