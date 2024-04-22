54f5d6837303fb926b5fa36269aad3f0c7dcbe08
CristobalTorresU <cristobal.torres.u@usach.cl>
2024-04-21 21:01:57 -0400

    Functionality 17 Fix

--- trigger_funcionality_17.sql
+++ trigger_funcionality_17.sql
@@ -14,30 +14,21 @@ RETURNS TRIGGER AS $$
 DECLARE
     currentDate DATE;
     currentTime TIME;
-    user_id_m INTEGER;
-    user_name_m TEXT;
     query_type_m TEXT;
 BEGIN
     currentDate := CURRENT_DATE;
     currentTime := CURRENT_TIME;
-    SELECT CURRENT_USER into user_name_m;
 
     IF TG_OP = 'INSERT' THEN
         query_type_m := 'Inserción';
-        user_id_m := NEW.user_id;
-        user_name_m := NEW.name;
     ELSIF TG_OP = 'UPDATE' THEN
         query_type_m = 'Actualización';
-        user_id_m := NEW.user_id;
-        user_name_m := NEW.name;
     ELSIF TG_OP = 'DELETE' THEN
         query_type_m = 'Eliminación';
-        user_id_m := OLD.user_id;
-        user_name_m := OLD.name;
     END IF;
 
-    INSERT INTO queries_log (user_id, user_name, call_date, call_time, query_type, query_statement)
-    VALUES (user_id_m, user_name_m, currentDate, currentTime, query_type_m, TG_SQL_STATEMENT::TEXT);
+    INSERT INTO queries_log (user_name, call_date, call_time, query_type, query_statement)
+    VALUES (CURRENT_USER, currentDate, currentTime, query_type_m, CURRENT_QUERY());
 
 	RETURN NULL;
 END;
